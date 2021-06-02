8e886e0be60659692f64b7122eab4733ea6fbd9f
Justin Whitehead <>
2021-06-02 02:07:36 +0000

    added infinite loop

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-kills = input("Enter kills:")
-print("kills are: " + kills) 
+while(1):
+  kills = input("Enter kills:")
+  print("kills are: " + kills) 
 
-kd = input("Enter kd:")
-print("kd is: " + kd) 
+  kd = input("Enter kd:")
+  print("kd is: " + kd) 
 
-deaths = float(kills)*(1/float(kd))
-print("deaths are " + str(deaths)) 
+  deaths = float(kills)*(1/float(kd))
+  print("deaths are " + str(deaths)) 
