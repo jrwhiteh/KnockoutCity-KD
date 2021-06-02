357817cc3734000b6031fabd68a4afde14c9d3bb
Justin Whitehead <whiteheadj90@gmail.com>
2021-06-01 22:13:23 -0400

    added try catch for ending program with ctrl c and not getting error

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-while(1):
-  kills = input("Enter kills:")
-  print("kills are: " + kills) 
+try:
+   while True:
+    kills = input("Enter kills:")
+    print("kills are: " + str(kills)) 
 
-  kd = input("Enter kd:")
-  print("kd is: " + kd) 
+    kd = input("Enter kd:")
+    print("kd is: " + str(kd)) 
 
-  deaths = float(kills)*(1/float(kd))
-  print("deaths are " + str(deaths)) 
+    deaths = float(kills)*(1/float(kd))
+    print("deaths are " + str(deaths))       
+except KeyboardInterrupt:
+    print("\n -Program Ending- ")
+
+
+
