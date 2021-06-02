38c045e52e3b92ff91efbed6d4d1b05cca5ee99b
Justin Whitehead <>
2021-06-02 02:05:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+kills = input("Enter kills:")
+print("kills are: " + kills) 
+
+kd = input("Enter kd:")
+print("kd is: " + kd) 
+
+deaths = float(kills)*(1/float(kd))
+print("deaths are " + str(deaths)) 
